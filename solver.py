f1641f882be0d7131e0e6a96ee494fd1c414264f
existelk <existelk@gmail.com>
2025-07-27 12:45:18 +0100

    Working row solver

--- solver.py
+++ solver.py
@@ -1,6 +1,8 @@
 import os
 import copy
 import time
+import sys
+from collections import Counter
 
 """
 [
@@ -14,13 +16,18 @@ import time
 PUZZLE_FILE = "puzzle.txt"
 CHUNK_SIZE = 3
 
-class PotentialList:
+class PotentialEntries:
     def __init__(self):
         self.valid_nums = [i for i in range (1, 10)]
 
     def remove_invalid(self, num) -> None:
+        if num not in self.valid_nums:
+            return
+
         self.valid_nums.remove(num)
 
+    def reset_valid_nums(self) -> None:
+        self.valid_nums = [i for i in range (1, 10)]
 
 class Sudoku:
     def __init__(self, puzzle_matrix: list, start: float = None, end: float = None):
@@ -30,7 +37,7 @@ class Sudoku:
         self._puzzle_end = end
 
         self.size = len(puzzle_matrix)
-        self.cell_options = [[PotentialList() for _ in range(self.size)] for _ in range(self.size)]
+        self.cell_options = [[PotentialEntries() for _ in range(self.size)] for _ in range(self.size)]
 
     def pretty_print(self, puzzle: list) -> None:
         """
@@ -63,7 +70,7 @@ class Sudoku:
     def determine_calculation_time(self) -> float:
         return (self._puzzle_end - self._puzzle_start)
     
-    def find_empty(self):
+    def find_empty(self) -> tuple[int, int]:
         for r in range(self.size):
             for c in range(self.size):
                 if (self.puzzle[r][c] == 0):
@@ -102,6 +109,10 @@ class Sudoku:
         original = self.get_unsolved_puzzle()
         self.puzzle = copy.deepcopy(original)
 
+        for r in range(self.size):
+            for c in range(self.size):
+                self.cell_options[r][c].reset_valid_nums()
+
         # reset timings as well
         self.set_start_time(0)
         self.set_end_time(0)
@@ -110,13 +121,52 @@ class Sudoku:
         for r in range(self.size):
             for c in range(self.size):
                 # only update valid_nums if the cell is empty
-                if self.puzzle[r][c] == 0:
-                    options = self.cell_options[r][c]
-                    options.valid_nums = [value for value in options.valid_nums if self.check_valid_entry(value, r, c)]
+                if self.puzzle[r][c] != 0:
+                    continue
 
-                    # only one valid option so add into puzzle
-                    if len(options.valid_nums) == 1: 
-                        self.puzzle[r][c] = options.valid_nums[0]
+                options = self.cell_options[r][c]
+                options.valid_nums = [value for value in options.valid_nums if self.check_valid_entry(value, r, c)]
+                # only one valid option so add into puzzle
+                if len(options.valid_nums) == 1: 
+                    cell_value = options.valid_nums[0]
+                    self.puzzle[r][c] = cell_value
+                    self.remove_element_valid_nums(r, c, cell_value)
+    
+    def remove_element_valid_nums(self, row_index: int, col_index: int, value: int) -> None:
+        """ When a new value is added to a cell, the related row and column cells valid entries has to be updated """
+        for r in range(self.size):
+            self.cell_options[r][col_index].remove_invalid(value)
+
+        for c in range(self.size):
+            self.cell_options[row_index][c].remove_invalid(value)
+
+    def row_solve_entries(self, row_index: int):
+        combined_possible_entries = []
+        history = {}
+        # iterate along the row
+        for c in range(self.size):
+            if self.puzzle[row_index][c] == 0:
+                options = self.cell_options[row_index][c]
+                combined_possible_entries += options.valid_nums
+                history[c] = options.valid_nums # keep a history of valid_nums and associated column index to avoid another loop later
+        
+        combined_possible_entries.sort()
+        counter = Counter(combined_possible_entries)
+        # if a possible value only appears for one cell in a row, it can be assigned to that cell
+        single_entries = [key for key, value in counter.items() if value == 1]
+
+        if len(single_entries) == 0:
+            return
+        
+        for key, value in history.items():
+            for elem in single_entries:
+                if elem not in value:
+                    continue
+
+                self.puzzle[row_index][key] = elem
+                single_entries.remove(elem)
+                if len(single_entries) == 0:
+                    break
 
 
 def solve_sudoku_original(puzzle: Sudoku, r: int = 0, c: int = 0) -> bool:
@@ -150,6 +200,18 @@ def improved_solve_sudoku(puzzle: Sudoku, r: int = 0, c: int = 0) -> bool:
     
     return False
 
+def personal_way_to_solve(sudoku : Sudoku) -> None:
+    # run valid_entries twice as some cells will be solved on the first pass
+    for _ in range(2):
+        sudoku.update_valid_entries()
+    sudoku.pretty_print(sudoku.puzzle)
+
+    # check if any rows can be solved
+    for r in range(sudoku.size):
+        sudoku.row_solve_entries(r)
+
+    # check if any columns can be solved
+
 
 def read_sudoku_puzzle(puzzle_file: str) -> Sudoku:
     """
@@ -157,7 +219,7 @@ def read_sudoku_puzzle(puzzle_file: str) -> Sudoku:
     """
     if not os.path.isfile(puzzle_file):
         print(f"File not found. Check file is in the expected location: {puzzle_file}")
-        exit(1)
+        sys.exit(1)
 
     puzzle = []
     with open("puzzle.txt") as f:
@@ -169,26 +231,42 @@ def read_sudoku_puzzle(puzzle_file: str) -> Sudoku:
 
 def main() -> None:
     sudoku_puzzle = read_sudoku_puzzle(PUZZLE_FILE)
+    sudoku_puzzle.pretty_print(sudoku_puzzle.puzzle)
+    print("\n\n")
 
     # solve sudoku with standard recursion and backtracking
-    start = time.perf_counter()
-    sudoku_puzzle.set_start_time(start) # using perf_countter as it provides a higher resolution time
-    solve_sudoku_original(sudoku_puzzle)
+    # start = time.perf_counter()
+    # sudoku_puzzle.set_start_time(start) # using perf_countter as it provides a higher resolution time
+    # solve_sudoku_original(sudoku_puzzle)
 
-    end = time.perf_counter() 
-    sudoku_puzzle.set_end_time(end)
-    sol_length = sudoku_puzzle.determine_calculation_time()
+    # end = time.perf_counter() 
+    # sudoku_puzzle.set_end_time(end)
+    # sol_length = sudoku_puzzle.determine_calculation_time()
 
-    print(f'The sudoku puzzle took {sol_length} seconds to find the solution: \n')
-    sudoku_puzzle.pretty_print(sudoku_puzzle.puzzle)
+    # print(f'The sudoku puzzle took {sol_length} seconds to find the solution: \n')
+    # sudoku_puzzle.pretty_print(sudoku_puzzle.puzzle)
 
-    # solve the sudoku with same method but only testing valid values in cells
-    sudoku_puzzle.reset_puzzle()
+    # # solve the sudoku with same method but only testing valid values in cells
+    # sudoku_puzzle.reset_puzzle()
+    # start = time.perf_counter()
+    # sudoku_puzzle.set_start_time(start)
+
+    # sudoku_puzzle.update_valid_entries()
+    # improved_solve_sudoku(sudoku_puzzle)
+
+    # end = time.perf_counter() 
+    # sudoku_puzzle.set_end_time(end)
+    # sol_length = sudoku_puzzle.determine_calculation_time()
+    
+    # print(f'The sudoku puzzle took {sol_length} seconds to find the solution: \n')
+    # sudoku_puzzle.pretty_print(sudoku_puzzle.puzzle)
+
+    # solve the sudoku my way
+    # sudoku_puzzle.reset_puzzle()
     start = time.perf_counter()
     sudoku_puzzle.set_start_time(start)
 
-    sudoku_puzzle.update_valid_entries()
-    improved_solve_sudoku(sudoku_puzzle)
+    personal_way_to_solve(sudoku_puzzle)
 
     end = time.perf_counter() 
     sudoku_puzzle.set_end_time(end)
@@ -196,7 +274,6 @@ def main() -> None:
     
     print(f'The sudoku puzzle took {sol_length} seconds to find the solution: \n')
     sudoku_puzzle.pretty_print(sudoku_puzzle.puzzle)
-
 
 if __name__ == "__main__":
     main()
